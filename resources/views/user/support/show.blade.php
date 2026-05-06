ca91e2db83f634be0644e5f47f5b7e18bb643ef0
salman khan <salmankhanm859@gmail.com>
2026-05-06 11:34:28 +0500

    fixes a small bug

--- resources/views/user/support/show.blade.php
+++ resources/views/user/support/show.blade.php
@@ -1,4 +1,4 @@
-wwww<x-user-layout :title="'Support Ticket #' . $ticket->id">
+<x-user-layout :title="'Support Ticket #' . $ticket->id">
     <div class="mb-6 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <div>
             <div class="flex items-center gap-3">
